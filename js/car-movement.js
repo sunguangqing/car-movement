37419c0f9092fcd4b1f04c210d20910d5e8782d6
sunguangqing <947912704@qq.com>
2018-05-28 14:39:27 +0800

    add car movement

---
+++ js/car-movement.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(".btn-group span").mousemove(function () {
+        var $index = $(this).index() - 1,
+            translate =  $index * 116,
+            rotate = $index * 720;
+        $(this).addClass("active").siblings("span").removeClass("active");
+        $(".car-box").css("transform", 'translateX(' + translate + 'px)').find("img").css("transform", 'rotate(' + rotate + 'deg)');
+    });
+});
