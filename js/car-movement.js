b4f1ce06c2fe12147c0cb2a5648ced75dad61d8b
sunguangqing <947912704@qq.com>
2018-05-28 14:41:10 +0800

    Update car-movement.js

--- js/car-movement.js
+++ js/car-movement.js
@@ -1,7 +1,7 @@
 $(function () {
     $(".btn-group span").mousemove(function () {
         var $index = $(this).index() - 1,
-            translate =  $index * 116,
+            translate =  $index * 115,
             rotate = $index * 720;
         $(this).addClass("active").siblings("span").removeClass("active");
         $(".car-box").css("transform", 'translateX(' + translate + 'px)').find("img").css("transform", 'rotate(' + rotate + 'deg)');
